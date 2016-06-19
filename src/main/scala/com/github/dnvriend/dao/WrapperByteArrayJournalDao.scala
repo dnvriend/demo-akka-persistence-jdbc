008e46db4ee9e8bf8ac651e35ce623842aa58c8f
Dennis Vriend <dnvriend@gmail.com>
2016-06-19 09:46:15 +0200

    upgraded to v2.4.0

---
+++ src/main/scala/com/github/dnvriend/dao/WrapperByteArrayJournalDao.scala
@@ -0,0 +1,11 @@
+package com.github.dnvriend.dao
+
+import akka.persistence.jdbc.dao.bytea.journal.ByteArrayJournalDao
+import akka.persistence.jdbc.journal.JdbcAsyncWriteJournal
+
+/**
+  * Created by dennis on 19-06-16.
+  */
+class WrapperByteArrayJournalDao extends ByteArrayJournalDao {
+
+}
